77a61eb4dc188cdf819221d971e83ab75f07c8b3
JackHaoyingZhou <zhouhaoying96@outlook.com>
2024-11-04 00:01:37 -0500

    update for PR

--- core/components/code/mtsRobotIO1394.cpp
+++ core/components/code/mtsRobotIO1394.cpp
@@ -325,16 +325,13 @@ void mtsRobotIO1394::Configure(const std::string & filename)
         jsonFile.close();
     }
 
-
     // Check firmware versions used so far
     if (!CheckFirmwareVersions()) {
         exit(EXIT_FAILURE);
     }
 
-
     // Read all the boards, a easy solution to the issue that specific board cannot be read when using boardcast-read-write
     mPort->ReadAllBoards();
-    
 }
 
 bool mtsRobotIO1394::SetupRobot(mtsRobot1394 * robot)
